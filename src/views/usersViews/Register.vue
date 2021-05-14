269b3c660d07566b31dc8950b81a8fa2f207ee8d
Florian <florian.simunek@gmail.com>
2021-05-14 17:16:34 +0200

    Add register system

--- src/views/usersViews/Register.vue
+++ src/views/usersViews/Register.vue
@@ -8,18 +8,35 @@
       <div class="user-system-form">
         <h2>S'inscrire</h2>
         <form action="" method="post">
+          <!-- TODO: @change on parent ? -->
           <div class="user-system-inputs">
             <input
               type="text"
               name="username"
               placeholder="Nom d'utilisateur"
+              ref="userUsername"
+              v-on:change="getUserInputValue()"
             />
-            <input type="text" name="mail" placeholder="Email" />
-            <input type="text" name="password" placeholder="Mot de passe" />
             <input
               type="text"
-              name="confirm-password"
+              name="mail"
+              placeholder="Email"
+              ref="userEmail"
+              v-on:change="getUserInputValue()"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Mot de passe"
+              ref="userPassword"
+              v-on:change="getUserInputValue()"
+            />
+            <input
+              type="password"
+              name="confirmPassword"
               placeholder="Confirmation mot de passe"
+              ref="userConfirmPassword"
+              v-on:change="getUserInputValue()"
             />
           </div>
           <div class="user-system-buttons">
@@ -32,8 +49,9 @@
             </router-link>
             <input
               class="user-system-buttons-register"
-              type="submit"
+              type="button"
               value="S'inscrire"
+              v-on:click="registerUser()"
             />
           </div>
         </form>
@@ -43,7 +61,36 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Register',
+  data() {
+    return {
+      user: {
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      },
+    };
+  },
+  methods: {
+    getUserInputValue() {
+      this.user.username = this.$refs.userUsername.value;
+      this.user.email = this.$refs.userEmail.value;
+      this.user.password = this.$refs.userPassword.value;
+      this.user.confirmPassword = this.$refs.userConfirmPassword.value;
+    },
+    registerUser() {
+      axios({
+        method: 'post',
+        url: `http://localhost:3000/api/user/register`,
+        data: this.user,
+      }).then(({ data }) => {
+        console.log(data);
+      });
+    },
+  },
 };
 </script>
