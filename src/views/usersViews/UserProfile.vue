e0252ce7183a09e1b1ea1ccb850e45aeb6af78e0
Florian <florian.simunek@gmail.com>
2021-05-18 16:09:13 +0200

    Stylized + add buttons to userProfile

--- src/views/usersViews/UserProfile.vue
+++ src/views/usersViews/UserProfile.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="home">
     <Layout />
+    <main>
+      <div class="userProfile">
+        <div class="userProfile-image"></div>
+        <div class="userProfile-informations">
+          <strong>Nom d'utilisateur:</strong> {{ this.user.username }} <br />
+          <strong>Adresse email:</strong> {{ this.user.email }} <br />
+          <strong>Création du compte :</strong>
+          {{
+            new Date(Date.parse(this.user.createdAt))
+              .toLocaleDateString('fr', {
+                hour: 'numeric',
+                minute: 'numeric',
+              })
+              .replace(':', 'h')
+          }}
+        </div>
+        <div class="userProfile-buttons">
+          <input
+            type="text"
+            value="Supprimer le compte"
+            class="btn btn-delete"
+          />
+          <input
+            type="text"
+            value="Modifier les informations"
+            class="btn btn-modify"
+          />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -49,6 +79,88 @@ export default {
       'sidebar main';
     max-height: 100vh;
     padding-top: 0;
+  }
+}
+
+main {
+  @media screen and (min-width: 992px) {
+    grid-area: main;
+    overflow-y: scroll;
+  }
+
+  /* Informations Bar */
+  #informations-bar {
+    background: $light-blue;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media screen and(min-width: 992px) {
+      //margin-left: 350px;
+    }
+
+    p {
+      color: #fff;
+      line-height: 1.3em;
+      margin: 0 10px;
+
+      &:nth-child(2) {
+        text-align: right;
+      }
+    }
+  }
+}
+
+.userProfile {
+  background: $darker-blue;
+  border-radius: 5px;
+  width: 50%;
+  margin: 20% auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &-image {
+    height: 100px;
+    width: 100px;
+    background: gray;
+    align-self: center;
+    margin: 20px;
+  }
+
+  &-informations {
+    align-self: center;
+    margin: 20px;
+    line-height: 1.5em;
+    color: white;
+  }
+
+  &-buttons {
+    text-align: center;
+    margin-bottom: 20px;
+    .btn {
+      width: 30%;
+      height: 30px;
+      color: white;
+      border: none;
+      border-radius: 2px;
+      margin: 0 5px;
+      font-size: 18px;
+      text-decoration: none;
+      transition: all 0.5s ease;
+      margin-top: 10px;
+      text-align: center;
+
+      &-delete {
+        background: #ff4747;
+      }
+
+      &-modify {
+        background: #03c946;
+      }
+    }
   }
 }
 </style>
